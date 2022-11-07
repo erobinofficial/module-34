1358736036995842f52c11f62c567666b32f78c6
erobinofficial <erobin8778@gmail.com>
2022-11-07 23:46:33 +0600

    quote added

--- JS/kanye.js
+++ JS/kanye.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+function loadQuotes() {
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuotes(data));
+}
+loadQuotes();
+function displayQuotes(quotes) {
+    const section = document.getElementById('quote');
+    const quote = document.createElement('p');
+    quote.innerHTML = quotes;
+    section.appendChild(quote);
+}
