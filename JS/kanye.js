dc4dc8d0eea8b4ffd0f6b74c11cab62275af2b89
erobinofficial <erobin8778@gmail.com>
2022-11-08 01:46:29 +0600

    almost

--- JS/kanye.js
+++ JS/kanye.js
@@ -1,14 +1,11 @@
-function loadQuotes() {
+const loadQuotes() {
     fetch('https://api.kanye.rest/')
     .then(res => res.json())
     .then(data => displayQuotes(data));
 }
-loadQuotes();
-function displayQuotes(quotes) {
+const displayQuotes = quotes => {
     const section = document.getElementById('quote');
-    for(const quote of quotes) {
-    const quote1 = document.createElement('p');
-    quote1.innerHTML = quote;
+    const quote = document.createElement('p');
+    quote.innerHTML = quotes.quote;
     section.appendChild(quote);
-    }
 }
