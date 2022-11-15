1204e8ad8a97b72fc5b5b9bd791a8ff409308910
erobinofficial <erobin8778@gmail.com>
2022-11-15 18:38:17 +0600

    more users

--- JS/friends.js
+++ JS/friends.js
@@ -1,5 +1,5 @@
 const loadFriends = () =>{
-    fetch('https://randomuser.me/api/?results=5')
+    fetch('https://randomuser.me/api/?results=5000')
     .then(res => res.json())
     .then(data => displayFriends(data));
 }
