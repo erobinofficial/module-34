146ca20742cc9eed5a123186a51b97acb686442e
erobinofficial <erobin8778@gmail.com>
2022-11-16 03:22:40 +0600

    cotation checked

--- JS/friends.js
+++ JS/friends.js
@@ -6,11 +6,11 @@ const loadFriends = () =>{
 loadFriends();
 const displayFriends = data => {
     const users = data.results;
-    const usersDiv = document.getElementById("friends");
+    const usersDiv = document.getElementById('friends');
     for(const user of users){
         console.log(user);
-        const p = document.createElement("p");
-        p.classList.add("user");
+        const p = document.createElement('p');
+        p.classList.add('user');
         p.innerText =  `Name: ${user.name.title} ${user.name.first} ${user.name.last}
         email: ${user.email}`;
         usersDiv.appendChild(p);
