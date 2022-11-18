2e770f861ee2af3e5be6cda1ad8fed5eec5470cc
erobinofficial <erobin8778@gmail.com>
2022-11-19 01:36:59 +0600

    added

---
+++ JS/canteen.js
@@ -0,0 +1,10 @@
+const searchFood = () =>{
+    const searchBox = document.getElementById('search-field');
+    const searchText = searchBox.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    searchBox.value = '';
+    fetch(url)
+       .then(response => response.json())
+       .then(data => console.log(data))
+    
+}
